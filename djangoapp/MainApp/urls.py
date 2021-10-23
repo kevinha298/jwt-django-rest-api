e3b4c96a8009be2426945354a2c33e2a246120ba
Kevin Ha <kevinha298@live.com>
2021-10-23 11:42:48 -0700

    Updated urls.py file in the MainApp folder.

--- djangoapp/MainApp/urls.py
+++ djangoapp/MainApp/urls.py
@@ -15,7 +15,12 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from django.conf.urls import url 
+
+from api.api import UserList, UserDetail
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    url(r'^api/users_list/$', UserList.as_view(), name='user_list'),
+    url(r'^api/users_list/(?P<employeeID>\d+)/$', UserDetail.as_view(), name='user_list'),
 ]
