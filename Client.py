be8baca7ad8506b0f6a08634f85901a2838b952d
Kevin Ha <kevinha298@live.com>
2021-11-13 10:48:57 -0800

    Added Client.py app for testing the api.

---
+++ Client.py
@@ -0,0 +1,33 @@
+import requests
+
+def get_token():
+    tokenEndPoint = f'{baseURL}/api/token/'
+    response = requests.post(tokenEndPoint, data={'username': 'admin', 'password': 'app123'})
+    token = response.json()['access']
+    return token
+
+#get data
+def get_data():
+    getEndPoint = f'{baseURL}/api/users_list/'
+    header = {'Authorization': f'Bearer {get_token()}'}
+    response = requests.get(getEndPoint, headers = header)
+    emp_data = response.json()
+    for e in emp_data:
+        print(e)
+
+#post(add) data
+def post_data(seq):
+    postEndPoint = f'{baseURL}/api/users_list/'
+    header = {'Authorization': f'Bearer {get_token()}'}
+    data = {
+        'employeeID': f'HQ00{seq}', 'employeeName': f'Test user {seq}', 'ranking': 6.8 + seq, 'age': 68 + seq
+    }
+    response = requests.post(postEndPoint, data = data, headers = header)
+    print(response.text, response.status_code)
+
+if __name__ == "__main__":
+    baseURL = 'http://127.0.0.1:8000'
+    # print(get_token())
+    post_data(11)
+    get_data()
+
